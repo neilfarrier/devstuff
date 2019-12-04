398c26da63e10c56c9b50e45d192bed11b98ab15
Neil Farrier <55524825+neilfarrier@users.noreply.github.com>
2019-12-05 10:43:20 +1100

    Add files via upload

--- Github Finder/app.js
+++ Github Finder/app.js
@@ -1,6 +1,9 @@
 //Init Github
 const github = new GitHub;
 
+//Init UI
+const ui = new UI;
+
 //Search input
 const searchUser = document.getElementById('searchUser');
 
@@ -15,13 +18,15 @@ searchUser.addEventListener('keyup', (e) => {
             .then(data => {
                 if (data.profile.message === 'Not Found') {
                     //Show alert
-
+                    ui.showAlert('User not found', 'alert alert-danger');
                 } else {
-                    //Show profile
+                    //show profile
+                    ui.showProfile(data.profile);
+                    ui.showRepos(data.repos);
                 }
             })
     } else {
         //Clear profile
-
+        ui.clearProfile();
     }
 });
--- Github Finder/github.js
+++ Github Finder/github.js
@@ -2,15 +2,21 @@ class GitHub {
     constructor() {
         this.client_id = 'f287bb713cf6ad2af73d';
         this.client_secret = '83c98dade108abb5a16cc18678a1140ce4966a99';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`http://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`http://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
